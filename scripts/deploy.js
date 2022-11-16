3faa376a4ba86000e1ec056de9df7d3c95f1e120
Silo Chad <silochad@protonmail.com>
2022-11-15 23:38:56 -0600

    update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,8 +2,26 @@ const { ethers, upgrades } = require("hardhat");
 const { mintEth } = require("../utils/mint");
 const { impersonateAccount, stopImpersonatingAccount } = require("../utils/signer");
 
+//// ADDRESSES
+// addressess of gnosis multi sigs for contract roles
+const multiSigAddress = "0x2bc95003C1f398aDA1eD0BbC9FE569a5489a0bb3";
+const gameAdminAddress = "0x44511a60E20E26e4B306AA250F334195B85d32B7";
+const adminAddress = "0x4dD35E0b1bE268C6f3A38eb9cACfCB22062b0Bc6";
+
+// address of ERC 20 token contract is using
+const usdcAddress = "0x07865c6E87B9F70255377e024ace6630C1Eaa37F";
+
+// address of the contract verasigner
+const signerAddress = "0x4dD35E0b1bE268C6f3A38eb9cACfCB22062b0Bc6";
+
+// address of the vault
+const vaultAddress = "0x3BB55aAfb1D3b8e8583cdA5935F0610e652feE2e";
+
+//// ROLES
 const MULTISIG_ROLE = "0xa5a0b70b385ff7611cd3840916bd08b10829e5bf9e6637cf79dd9a427fc0e2ab"
 const GAME_ADMIN_ROLE = "0x9b7946abd96dccbe6cfc6cc2c13300ab429d93e16fa72dc459eeccda73817f08"
+const ADMIN_ROLE = "0xa49807205ce4d355092ef5a8a18f56e8913cf4a201fbe287825b095693c21775"
+const DEFAULT_ADMIN_ROLE = "0x1effbbff9c66c5e59634f24fe842750c60d18891155c32dd155fc2d661a4c86d"
 
 /**
  * Deploy betting & admin.
@@ -15,13 +33,25 @@ const GAME_ADMIN_ROLE = "0x9b7946abd96dccbe6cfc6cc2c13300ab429d93e16fa72dc459eec
  * @param {*} signerAddress
  */
 async function main(
-    impersonate = false,
-    erc20Address = "0x07865c6e87b9f70255377e024ace6630c1eaa37f", // could be any erc20
+  impersonate = false,
+  erc20Address
 ) {
-  // Get address that is deploying
-  const [deployer] = await ethers.getSigners();
-  const deployerAddress = deployer.address;
+  if (!erc20Address) throw new Error("Specify an erc20Address to deploy");
 
+  console.log("Deploying Paradox admin for erc20Address", erc20Address);
+
+  // Get address that is deploying
+  let deployer;
+  let deployerAddress;
+  if (impersonate) {
+    deployerAddress = "0x8A974Aa04c34AC66ac16F2c7112aEDDaDB4F87A1"
+    deployer = await impersonateAccount(deployerAddress)
+  } else {
+    [deployer] = await ethers.getSigners();
+    deployerAddress = deployer.address;
+  }
+
+  // DEPLOY PROXY AND CLONE FOR ERC1155 MINTER
   const ERC1155PresetMinterPauser = await ethers.getContractFactory("ERC1155PresetMinterPauser");
   const erc1155 = await ERC1155PresetMinterPauser.deploy("Paradox Minter");
   await erc1155.deployed();
@@ -30,12 +60,16 @@ async function main(
   const Proxy = await ethers.getContractFactory("Proxy");
   const proxy = await Proxy.deploy(erc1155.address);
   await proxy.deployed();
-
   console.log("Proxy deployed at: ", proxy.address);
 
   // DEPLOY ADMIN CONTRACT
   const BettingAdmin = await ethers.getContractFactory("BettingAdmin");
-  const bettingAdmin = await upgrades.deployProxy(BettingAdmin, [deployerAddress, deployerAddress, deployerAddress, proxy.address])
+  const bettingAdmin = await upgrades.deployProxy(BettingAdmin, [
+    erc20Address, // token
+    deployerAddress, // vaultAddress, // vault
+    deployerAddress, // signerAddress, // signer
+    proxy.address
+  ])
   await bettingAdmin.deployed();
   console.log("BettingAdmin deployed to:", bettingAdmin.address);
 
@@ -46,14 +80,18 @@ async function main(
   ]);
   await betting.deployed();
 
+  // UPDATE ROLES
   await bettingAdmin.grantRole(MULTISIG_ROLE, deployerAddress);
   console.log('MULTISIG_ROLE GRANTED');
   await bettingAdmin.grantRole(GAME_ADMIN_ROLE, deployerAddress);
   console.log('GAME_ADMIN_ROLE GRANTED');
+  await bettingAdmin.grantRole(ADMIN_ROLE, deployerAddress);
+  console.log('ADMIN_ROLE GRANTED');
 
   console.log("BettingAdmin deployed to:", bettingAdmin.address);
   console.log("Deployed by:", await bettingAdmin.signer.getAddress());
   console.log("");
+
   if (impersonate === true) {
     console.log("Impersonating signer to update betting address.")
     const signer = await impersonateAccount(deployerAddress)
--- scripts/deploy_local.js
+++ scripts/deploy_local.js
@@ -1,6 +1,6 @@
 const { main } = require('./deploy');
 
-main(true).catch((error) => {
+main(true, "0x77700005BEA4DE0A78b956517f099260C2CA9a26").catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
